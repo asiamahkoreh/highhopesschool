7db74f1ce7c19dcfb27e27eb06fb6216db5be11f
asiamahkoreh <asiamahkorehglobal@gmail.com>
2023-08-14 15:21:22 +0000

    change nav-toggle div to button and add css

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
+const nav = document.querySelector("nav");
 const navLinks = document.querySelector("#nav-links");
 const navToggle = document.querySelector("#nav-toggle");
+
+
+navToggle.addEventListener("click", showMobileNavLinks);
+
+function showMobileNavLinks() {
+
+}
